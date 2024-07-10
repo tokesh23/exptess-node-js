5876e07b888e09f9500d87b00dda24649c03c756
tokesh23 <tokesh1998@gmail.com>
2024-07-10 21:41:01 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express =require('express')
+
+const upddate=()=>{
+    const app=express()
+ 
+
+    app.listen(()=>{
+        console.log(`server connection the bulid localhost PORT:{PORT}`)
+
+    })
+  
+}
+upddate()
